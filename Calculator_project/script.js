f79ee94127c3553705a6c8d5cb760d4c618edb11
Kalupx <jefte.paula@ufv.br>
2023-01-27 12:40:27 -0300

    botoes e clear implementados

---
+++ Calculator_project/script.js
@@ -0,0 +1,6 @@
+function getNumber(number){
+    document.getElementById('calculadora-tela').value += number;
+}
+function clearFunction(){
+    document.getElementById('calculadora-tela').value = '';
+}
