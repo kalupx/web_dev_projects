026a6341356fd915b38a28a48fc208bcd585fe0b
Kalupx <jefte.paula@ufv.br>
2023-01-28 14:45:50 -0300

    bug-fix com eval

--- Calculator_project/script.js
+++ Calculator_project/script.js
@@ -1,5 +1,18 @@
 function calcular(tipo, valor){
-
+    if(tipo === 'acao'){
+        if(valor === 'c') clear();
+        if(valor === '+' || valor === '-' || valor === '*' || valor === '/'){
+            document.getElementById('calculadora-tela').value += valor;
+        }
+        if(valor === '='){
+            document.getElementById('calculadora-tela').value = eval(document.getElementById('calculadora-tela').value);
+        }
+    }else if(tipo === 'valor'){
+        document.getElementById('calculadora-tela').value += valor;
+    }else{
+        //exception handling
+        alert();
+    }
 }
 
 function clear(){
