804cb10c9474886a161bed6149dcd3b29b5b3d62
Kalupx <jefte.paula@ufv.br>
2023-01-27 23:01:35 -0300

    calculadora funcionando, falta bug fix

--- Calculator_project/script.js
+++ Calculator_project/script.js
@@ -4,26 +4,37 @@ function getNumber(number){
 
 function clearFunction(){
     document.getElementById('calculadora-tela').value = '';
+    
 }
 
-var number_2_bool =false;
 var number_1 = null;
 var number_2 = null;
-var resp = 0;
-function operation(){
-    if(operation === '=' && number_2 === null){
-        resp = number_1;
-    }else{
-        if(!number_2_bool){
-            number_1 = parseFloat(document.getElementById('calculadora-tela').value);
-            number_2_bool = true;
-            console.log(`o numero 1 é ${number_1}`)
-        }else{
-            number_2 = parseFloat(document.getElementById('calculadora-tela').value);
-            number_2_bool = false;
-            console.log(`o numero 2 é ${number_2}`)
-        }
+var resp = null;
+var oldSign = null;
+function operation(opSign){
+    if(opSign != '='){
+        oldSign = opSign;
+        number_1 = parseFloat(document.getElementById('calculadora-tela').value);
+    }else if(opSign ==='='){
+        number_2 = parseFloat(document.getElementById('calculadora-tela').value);
+        switch(oldSign){
+            case '+':
+                resp = number_1 + number_2;
+                break;
+            case '-':
+                resp = number_1 - number_2;
+                break;
+            case '*':
+                resp = number_1 * number_2;
+                break;
+            case '/':
+                resp = number_1 / number_2;
+                break;
+            default:
+                //inserir exception handling
+                break;
+                    }
     }
     clearFunction();
-    alert(resp);
+    document.getElementById('calculadora-tela').value = resp;
 }
