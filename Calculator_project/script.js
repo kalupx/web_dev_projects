dddb27019ca530b8fb625b8d37bb76340fd4bc01
Kalupx <jefte.paula@ufv.br>
2023-01-28 14:26:43 -0300

    mudancas no chamado da funcao no indice e js

--- Calculator_project/script.js
+++ Calculator_project/script.js
@@ -1,40 +1,7 @@
-function getNumber(number){
-    document.getElementById('calculadora-tela').value += number;
+function calcular(tipo, valor){
+
 }
 
-function clearFunction(){
+function clear(){
     document.getElementById('calculadora-tela').value = '';
-    
-}
-
-var number_1 = null;
-var number_2 = null;
-var resp = null;
-var oldSign = null;
-function operation(opSign){
-    if(opSign != '='){
-        oldSign = opSign;
-        number_1 = parseFloat(document.getElementById('calculadora-tela').value);
-    }else if(opSign ==='='){
-        number_2 = parseFloat(document.getElementById('calculadora-tela').value);
-        switch(oldSign){
-            case '+':
-                resp = number_1 + number_2;
-                break;
-            case '-':
-                resp = number_1 - number_2;
-                break;
-            case '*':
-                resp = number_1 * number_2;
-                break;
-            case '/':
-                resp = number_1 / number_2;
-                break;
-            default:
-                //inserir exception handling
-                break;
-                    }
-    }
-    clearFunction();
-    document.getElementById('calculadora-tela').value = resp;
 }
