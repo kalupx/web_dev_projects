8166c7ec6b534c1acb975d6bc57f9eb209c615dc
Kalupx <jefte.paula@ufv.br>
2023-01-30 16:15:22 -0300

    implementacao da tabela dinamica

--- orçamento-pessoal-project/script.js
+++ orçamento-pessoal-project/script.js
@@ -90,5 +90,34 @@ function carregaListaDespesas(){
 
     despesas = bd.recuperarTodosRegistros();
 
-    console.log(despesas);
+    let listaDespesas = document.getElementById('listaDespesas');
+    
+    despesas.forEach(function(d){
+        let linha = listaDespesas.insertRow();
+
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
+        switch(d.tipo){
+            case '1': 
+                d.tipo = 'alimentação';
+                break;
+            case '2':
+                d.tipo = 'educação';
+                break;
+            case '3':
+                d.tipo = 'lazer';
+                break;
+            case '4':
+                d.tipo = 'saúde';
+                break;
+            case '5':
+                d.tipo = 'transporte';
+                break;
+            default:
+                //excpetion handling
+                break;
+        }
+        linha.insertCell(1).innerHTML = d.tipo;
+        linha.insertCell(2).innerHTML = d.descricao;
+        linha.insertCell(3).innerHTML = d.valor;
+    })
 }
