0f2a3511d21968fc38f6ede44207975d969b3a23
Kalupx <jefte.paula@ufv.br>
2023-01-30 14:00:25 -0300

    refactory do modal

--- orçamento-pessoal-project/script.js
+++ orçamento-pessoal-project/script.js
@@ -57,8 +57,19 @@ function cadastrarDespesa(){
     
     if(despesa.validarDados()){
         bd.gravar(despesa);
-        $('#sucessoGravacao').modal('show');
+        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso';
+        document.getElementById('modal_itulo_div').className = 'modal-header text-success';
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa inserida com sucesso';
+        document.getElementById('modal_btn').innerHTML = 'Voltar';
+        document.getElementById('modal_btn').className = 'btn btn-success';
+        $('#modalRegistraDespesa').modal('show');
     }else{
-        $('#erroGravacao').modal('show');
+        document.getElementById('modal_titulo').innerHTML = 'Erro na Gravação';
+        document.getElementById('modal_itulo_div').className = 'modal-header text-danger';
+        document.getElementById('modal_conteudo').innerHTML = 'Por favor, preencha todos os campos';
+        document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir';
+        document.getElementById('modal_btn').className = 'btn btn-danger';
+        $('#modalRegistraDespesa').modal('show');
     }
 }
+/* Por favor, preencha todos os campos */
