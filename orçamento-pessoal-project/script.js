cd2c600cb5d818c756fc582c99ce88a22be0439e
Kalupx <jefte.paula@ufv.br>
2023-01-31 09:31:33 -0300

    implementacao do metodo de filtragem

--- orçamento-pessoal-project/script.js
+++ orçamento-pessoal-project/script.js
@@ -46,6 +46,36 @@ class Bd
         }
         return despesas;
     }
+
+    pesquisar(despesa){
+        let despesasFiltradas = Array();
+        despesasFiltradas = this.recuperarTodosRegistros();
+        
+        if(despesa.ano != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano);
+        }
+
+        if(despesa.mes != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes);
+        }
+
+        if(despesa.dia != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia);
+        }
+
+        if(despesa.tipo != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo);
+        }
+
+        if(despesa.descricao != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao);
+        }
+
+        if(despesa.valor != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor);
+        }
+        console.log(despesasFiltradas);
+    }
 }
 
 let bd = new Bd();
@@ -126,4 +156,17 @@ function carregaListaDespesas(){
         linha.insertCell(2).innerHTML = d.descricao;
         linha.insertCell(3).innerHTML = d.valor;
     })
+}
+
+function pesquisarDespesa(){
+    let ano = document.getElementById('ano').value;
+    let mes = document.getElementById('mes').value;
+    let dia = document.getElementById('dia').value;
+    let tipo = document.getElementById('tipo').value;
+    let descricao = document.getElementById('descricao').value;
+    let valor = document.getElementById('valor').value;
+   
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor);
+
+    bd.pesquisar(despesa);
 }
